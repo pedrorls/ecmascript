8228d5ca2792e09ffa9c9076f6f0f1bb443d286a
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-15 14:18:01 -0300

    Lexical this

--- app/js/index.js
+++ app/js/index.js
@@ -9,19 +9,15 @@ import Animal from './animal';
 logTitle('Title');
 /* coding examples */
 
-const hello = () => {
-  const es6 = 'ES6';
-  return `Hello ${es6}`;
-};
+const person = {
+  name: 'Pedro',
+  cars: ['NewCivic', 'Cruze Hatch' ],
+  toString: function() {
+    //log(`${this.name} has ${this.cars}`)
+    this.cars.forEach(car => {
+      log(`${this.name} has ${car}`);
+    });
+  }
+}
 
-const powers = [1,2,3,4,5].map((number, index) => Math.pow(number, index));
-
-
-const add = (n1, n2) => n1 + n2;
-
-const milesToKm = (miles) => miles * 1.60934;
-
-log(hello());
-log(powers);
-log(add(100,200));
-log(milesToKm(100));
+person.toString();
