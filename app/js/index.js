bfca5bf29ccaf0112c00432a23bd62cd50d047b5
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-15 09:21:49 -0300

    exports and imports

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,11 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
+import * as math from './math';
 logTitle('Title');
 /* coding examples */
+log(math.add(2,2));
+log(math.sub(2,2));
+log(math.div(2,2));
+log(math.mult(2,2));
+log(math.PI);
---
+++ app/js/math.js
@@ -0,0 +1,17 @@
+export var add = function(n1, n2) {
+	return n1 + n2
+}
+
+export var div = function(n1, n2) {
+	return n1 / n2
+}
+
+export var sub = function(n1, n2) {
+	return n1 - n2
+}
+
+export var mult = function(n1, n2) {
+	return n1 * n2
+}
+
+export var PI = 3.1415
