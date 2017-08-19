b1d3a8cfff78b0f199636c92eb90dd7628181bf0
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-18 23:31:20 -0300

    destructuring the object

--- app/js/index.js
+++ app/js/index.js
@@ -8,9 +8,36 @@ import * as math from './math';
 import Animal from './animal';
 logTitle('Title');
 /* coding examples */
-const names = ['Chandler', 'Monica', 'Joe', 'Phoebe', 'Ross', 'Rachel'];
-// const chandler = names[0];
-// const monica = names[1];
-// const joe = names[2]
-const [a, b , c] = names;
-log(`${a} ${b} ${c}`)
+const getUser = () => {
+    return {
+      name: 'John',
+      surname: 'Doe',
+      gender: 'male',
+      address: {
+        country: 'United States',
+        city: 'California',
+        postCode: 'CA',
+        fullAddress: {
+          doorNumber: 22,
+          street: 'LA st'
+        }
+      },
+      age: 29
+    }
+  };
+  
+  const user = getUser();
+  
+//   const name = user.name;
+//   const age = user.age;
+//   const country = user.address.country;
+  const doorNumber = user.address.fullAddress.doorNumber;
+
+  const {name , age, address : {country : theCountry} } = user;
+  const {address: {fullAddress : {doorNumber: number}}} = user;
+
+  log(name);
+  log(age);
+  log(theCountry);
+  log(number);
+  
