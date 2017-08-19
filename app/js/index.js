b0fb4f775f4ad2debcd50c6e3fec72a230da8b66
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-18 23:06:15 -0300

    object

--- app/js/index.js
+++ app/js/index.js
@@ -8,20 +8,9 @@ import * as math from './math';
 import Animal from './animal';
 logTitle('Title');
 /* coding examples */
-const pricePropName = "PRICE";
-
-const calculator = (name, price) => {
-  return{
-    name,
-    add(n1, n2){
-      return n1 + n2
-    },
-    [pricePropName.toLowerCase()]: price
-  }
-}
-
-const calc = calculator('Casio', '9.99');
-
-log(calc.name)
-log(calc.add(10, 33))
-log(calc.price)
+const names = ['Chandler', 'Monica', 'Joe', 'Phoebe', 'Ross', 'Rachel'];
+// const chandler = names[0];
+// const monica = names[1];
+// const joe = names[2]
+const [a, b , c] = names;
+log(`${a} ${b} ${c}`)
