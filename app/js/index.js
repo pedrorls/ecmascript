fa62d39c79948ccaf076477fe686bc563427a082
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-18 23:35:26 -0300

    Function default parameters

--- app/js/index.js
+++ app/js/index.js
@@ -8,36 +8,10 @@ import * as math from './math';
 import Animal from './animal';
 logTitle('Title');
 /* coding examples */
-const getUser = () => {
-    return {
-      name: 'John',
-      surname: 'Doe',
-      gender: 'male',
-      address: {
-        country: 'United States',
-        city: 'California',
-        postCode: 'CA',
-        fullAddress: {
-          doorNumber: 22,
-          street: 'LA st'
-        }
-      },
-      age: 29
-    }
-  };
-  
-  const user = getUser();
-  
-//   const name = user.name;
-//   const age = user.age;
-//   const country = user.address.country;
-  const doorNumber = user.address.fullAddress.doorNumber;
+const calculatePay = (yrSalary, bonus = {
+    teamBonus: 0,
+    emplyeeBonus: 0
+}) =>
+    yrSalary + bonus.teamBonus + bonus.emplyeeBonus;
 
-  const {name , age, address : {country : theCountry} } = user;
-  const {address: {fullAddress : {doorNumber: number}}} = user;
-
-  log(name);
-  log(age);
-  log(theCountry);
-  log(number);
-  
+log(calculatePay(22000, {teamBonus: 10000, emplyeeBonus: 6000}));
