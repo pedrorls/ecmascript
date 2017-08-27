29d2108089df72b8f972b7d99dd965525c3188bb
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-27 19:32:06 -0300

    Promises.all

--- app/js/index.js
+++ app/js/index.js
@@ -8,9 +8,9 @@ import * as math from './math';
 logTitle('Title');
 /* coding examples */
 
-const promise = new Promise((resolve, reject) => {
+const namesPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve("Data back from server");
+        resolve(["Alex, Mel, Melanie, Valentina"]);
     }, 3000);
 
     setTimeout(() =>{
@@ -18,8 +18,22 @@ const promise = new Promise((resolve, reject) => {
     }, 4000);
 });
 
-promise.then(response =>{
-    log(response);
-}).catch(error =>{
+const surnamesPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(["Cunha, Silva, C, Rodrigues"]);
+    }, 3000);
+
+    setTimeout(() =>{
+        reject("No data back from server, error was found")
+    }, 4000);
+});
+
+
+Promise.all([namesPromise, surnamesPromise]).then(data => {
+    const [names, surnames] = data;
+    for (var i = 0; i < names.length; i++){ 
+        log(`${names[i]} ${surnames[i]}`);
+    }
+}).catch(error => {
     log(error);
 });
