335140f803c7ffd64c45fc5af354a8898a8280a8
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-27 20:06:47 -0300

    Generators

--- app/js/index.js
+++ app/js/index.js
@@ -8,17 +8,22 @@ import * as math from './math';
 logTitle('Title');
 /* coding examples */
 
-const getRandomUsers = n => {
-    const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`);
-    fetchRandomUsers.then(data => {
-        data.json().then(randomUsers => {
-            log(JSON.stringify(randomUsers.results.length));
-            randomUsers.results.forEach(user => {
-                const {gender, email} = user;
-                log(`${gender} - ${email}`);
-            });
-        });
-    });
+const getNums = function* (numbers){
+    for(var i = 0; i < numbers.length; i++){
+        yield numbers[i];
+    }
 }
 
-getRandomUsers(50);
+
+const getNumsGen = getNums([1,2,3,4,5]);
+
+const interval = setInterval(() => {
+    const next = getNumsGen.next();
+    if(next.done){
+        log("Done");
+        clearInterval(interval);
+    }else {
+        const num = next.value;
+        log(num)
+    }
+}, 1000)
