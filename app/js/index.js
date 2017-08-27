eec2d3a9f08f7442fa6d08402fe6199f3ef97a8b
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-27 19:52:19 -0300

    Promises with fetch (real example)

--- app/js/index.js
+++ app/js/index.js
@@ -8,32 +8,17 @@ import * as math from './math';
 logTitle('Title');
 /* coding examples */
 
-const namesPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(["Alex, Mel, Melanie, Valentina"]);
-    }, 3000);
+const getRandomUsers = n => {
+    const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`);
+    fetchRandomUsers.then(data => {
+        data.json().then(randomUsers => {
+            log(JSON.stringify(randomUsers.results.length));
+            randomUsers.results.forEach(user => {
+                const {gender, email} = user;
+                log(`${gender} - ${email}`);
+            });
+        });
+    });
+}
 
-    setTimeout(() =>{
-        reject("No data back from server, error was found")
-    }, 4000);
-});
-
-const surnamesPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve(["Cunha, Silva, C, Rodrigues"]);
-    }, 3000);
-
-    setTimeout(() =>{
-        reject("No data back from server, error was found")
-    }, 4000);
-});
-
-
-Promise.all([namesPromise, surnamesPromise]).then(data => {
-    const [names, surnames] = data;
-    for (var i = 0; i < names.length; i++){ 
-        log(`${names[i]} ${surnames[i]}`);
-    }
-}).catch(error => {
-    log(error);
-});
+getRandomUsers(50);
