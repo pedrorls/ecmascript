943767afb824158c526b79848d8b850231f2740d
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-29 12:01:13 -0300

    Promises and Generators with bluebird

--- app/js/index.js
+++ app/js/index.js
@@ -5,20 +5,33 @@ import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
 import * as math from './math';
+import {coroutine as co} from 'bluebird';
 logTitle('Title');
 /* coding examples */
 
-const getRandomUsers = n => {
-    const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`)
-    fetchRandomUsers.then(data => {
-      data.json().then(randomUsers => {
-        log(JSON.stringify(randomUsers.results.length));
-        randomUsers.results.forEach(user => {
-          const {gender, email} = user;
-          log(`${gender} - ${email}`);
-        });
-      })
-    });
-  }
-  
-  getRandomUsers(100);
+// const getRandomUsers = n => {
+//     const fetchRandomUsers = fetch(`https://randomuser.me/api/?results=${n}`)
+//     fetchRandomUsers.then(data => {
+//       data.json().then(randomUsers => {
+//         log(JSON.stringify(randomUsers.results.length));
+//         randomUsers.results.forEach(user => {
+//           const {gender, email} = user;
+//           log(`${gender} - ${email}`);
+//         });
+//       })
+//     });
+//   }
+
+
+const getRandUsers = co(function* (n){
+  const fetchRandomUsers = yield fetch(`https://randomuser.me/api/?results=${n}`);
+  const data = yield fetchRandomUsers.json();
+  return data;
+});
+
+getRandUsers(10).then(randomUsers => {
+  randomUsers.results.forEach(user => {
+    const {gender, email} = user;
+    log(`${gender} - ${email}`);
+  });
+}).catch(err => log);
