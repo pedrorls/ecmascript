dd9e01351f82c0fb5d721b704bd174748ed902f6
Pedro Rodrigues Lima <pedro.rls93@gmail.com>
2017-08-29 14:59:06 -0300

    last commit

--- app/js/index.js
+++ app/js/index.js
@@ -8,18 +8,3 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 logTitle('Title');
-
-class MainComponent extends Component {
-  constructor(props){
-    super(props);
-  }
-  render(){
-    return(
-      <h1>Hello World !</h1>
-    );
-  }
-}
-
-const mountNode = document.getElementById('mountNode');
-
-render(<MainComponent/>, mountNode);
